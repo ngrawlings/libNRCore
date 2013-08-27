f2c0ab06a735b1bb4f51ec10b3a59249339adc5e
ngrawlings <ngrawlings@gmail.com>
2013-08-27 20:46:44 +0200

    Corrected a mistake with the socket opt len not being a writable address.

--- libnrcore/socket/Socket.cpp
+++ libnrcore/socket/Socket.cpp
@@ -226,7 +226,8 @@ namespace nrcore {
                 // This is purely to report a situation that should never occure, but I think there is a race condition that leads to this state
                 // by reporting the socket state when this occures will provide an insight to help avoid fd escalations.
                 int res;
-                getsockopt(fd, SOL_SOCKET, SO_ERROR, &res, ((socklen_t*)sizeof(res)));
+                socklen_t sock_len = sizeof(res);
+                getsockopt(fd, SOL_SOCKET, SO_ERROR, &res, &sock_len);
                 logger.log(Log::LOGLEVEL_ERROR, "Socket Alreadty Closed -> fd %d with errno %d", fd, res);
             }
 
